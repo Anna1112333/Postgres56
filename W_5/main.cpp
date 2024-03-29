650da4766ecc18e97263ae26fbd8e0bdea2f8579
Anna1112333 <anna_1112333@mail.ru>
2024-03-29 21:47:37 +0500

    у меня id  на serial

--- W_5/main.cpp
+++ W_5/main.cpp
@@ -2,7 +2,7 @@
 #include <tuple>
 #include <pqxx/pqxx>
 #include <Windows.h>
-#pragma execution_character_set( "utf-8" )
+//#pragma execution_character_set( "utf-8" )
 
 class table {
 public:
@@ -23,7 +23,8 @@ public:
 			std::cout << "Напишите название таблицы: ";
 			std::cin >> new_table;
 
-			pqxx::result r = t.exec("create table public." + new_table + " (user_id int4, email varchar);");
+			pqxx::result r = t.exec("create table public." + new_table + " (user_id int4, nane varchar(20),"
+				"soname varchar(20), email varchar, phone_number int4);");
 					}
 		t.commit();
 		std::cout << "Ваша таблица "
@@ -31,7 +32,14 @@ public:
 			<<" создана и в ней есть поля по умолчанию:user_id, name, soname, email, phone_number\n";
 		
 	}
-	void insert_client(){}
+	void insert_client(){//работает
+		pqxx::result r = t.exec("insert into users (user_id, name, soname) values (12,'Vitor', 'Biriukow');"
+		"insert into us_email (user_id_e, email) values (12, 'as@yandex');"
+		"insert into us_phone (user_id_ph, phone_number) values (12, 123789);");
+	
+	t.commit();
+	
+	}
 	void insert_phine(){}
 	void change_data_of_client(){}
 	void delete_phone(){}
@@ -39,32 +47,26 @@ public:
 
 	}
 	void faind_client(int r){
-		setlocale(LC_ALL, "RUS");
-		/*for (std::tuple<int, std::string, std::string, std::string, long long int>q1 : t.query<int,
-		std::string, std::string, std::string, long long int>("select user_id, name, soname, email, phone_number "
-			"from (select * from users"
-			" left join us_email on users.user_id=us_email.user_id_e) v"
-			" left join us_phone on v.user_id=us_phone.user_id_ph order by user_id;"))
-			std::cout << "user_id = " << std::get<0>(q1) << std::endl;
-			*/
+		
 		for (auto [user_id, name, soname, email, phone_number] : t.query
 			<int, std::string, std::string, std::string, long long int>
-			("select user_id, name, soname, email, phone_number from "
+			("select user_id, coalesce(name, ' ') as name, coalesce(soname, ' ') as soname, "
+				"coalesce(email, ' ') as email,  coalesce(phone_number, 0) from "
 				"(select * from users left join us_email on users.user_id=us_email.user_id_e) v "
 				"left join us_phone on v.user_id=us_phone.user_id_ph order by user_id;"))
 		{
 			
 			std::cout << "user_id = " << user_id << "   name = " << name << "   soname = "
-				<< soname << "   email = " << email << "   phone_number= " << phone_number << std::endl;
+				<< soname << "   email = E" << email << "   phone_number= " << phone_number << std::endl;
 		}
 	}
 };
 int main()
 {
 	
-	//SetConsoleCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
 SetConsoleOutputCP(CP_UTF8);
-	//setvbuf(stdout, nullptr, _IOFBF, 1000);
+	setvbuf(stdout, nullptr, _IOFBF, 1000);
 	
 	setlocale(LC_ALL, "RUS");	 
 
@@ -76,21 +78,21 @@ SetConsoleOutputCP(CP_UTF8);
 			"user=pq "
 			"password=zxc123");
 		std::cout << "base exist /n";
-	table a(c);	
-	a.create_table();
-	int i=0;
-	{             //по русски не выводит
+	   table a(c);	
+	   //a.create_table();
+	   int i=0;
+	   {             //по русски не выводит
 		std::cout << "Insert number for search:  1 by name, 2 by soname, 3 by phone number, 4 by email: ";
-		//setlocale(LC_ALL, "UTF8");
-	}
-	while (i > 4 || i < 1)
-	{
+		}
+	   while (i > 4 || i < 1)
+	   {
 		std::cin >> i;
 		if (i > 4 || i < 1)
 			std::cout << "Number was wrong, it should be 1, 2, 3 or 4: ";
-	}
+	   }
 
-	//a.faind_client(i);	
+	    a.faind_client(i);
+		a.insert_client();
 	
 
 
